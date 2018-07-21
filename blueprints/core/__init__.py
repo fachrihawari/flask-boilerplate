c5e10d985e0980015413d147cadadaaf5fec890b
Fachri Hawari <fachri.hawari@gmail.com>
2018-07-21 15:11:08 +0700

    setup adminlte v3 alpha template for admin

--- blueprints/core/__init__.py
+++ blueprints/core/__init__.py
@@ -1,14 +1,20 @@
 # Load external module
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # Load models from this module
 from .models import *
 
 # Create blueprint instance
-core = Blueprint('core', __name__, url_prefix='/')
+core = Blueprint('core', __name__, url_prefix='/', template_folder='templates/')
 
-# Show core
-@core.route('/', defaults={'page': 'index'})
-@core.route('/<string:page>')
-def show(page):
-    return 'ini adalah page: %r' % page
+# Index Page
+@core.route('/')
+def index():
+    return '''welcome to flask boilerplate
+        <a href="/admin/">Go to admin</a>
+    '''
+
+# Index Admin Page
+@core.route('/admin/')
+def admin_index():
+    return render_template('admin/welcome.html')
