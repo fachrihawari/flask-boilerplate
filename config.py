f7eff3ede091beb831d34ad158aa08c49d7b9142
Fachri Hawari <fachri.hawari@gmail.com>
2018-08-07 15:13:20 +0700

    add config file

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-SECRET_KEY = 'Your secret key'
-DEBUG = False
+from os import getenv
+
+APPLICATION_NAME = getenv('APPLICATION_NAME', 'Flask Boilerplate')
+
+SECRET_KEY = getenv('SECRET_KEY', 'Your Secret Key')
+DEBUG = getenv('DEBUG', True)
 
 # SQLAlchemy config
-SQLALCHEMY_ECHO = False
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@host/dbname'
+SQLALCHEMY_ECHO = getenv('SQLALCHEMY_ECHO', False)
+SQLALCHEMY_TRACK_MODIFICATIONS = getenv('SQLALCHEMY_TRACK_MODIFICATIONS', True)
+SQLALCHEMY_DATABASE_URI = getenv('SQLALCHEMY_DATABASE_URI', 'postgresql://username:password@host/dbname')
 
 # Your Blueprint 
 BLUEPRINT_DIRECTORY = 'blueprints'
 BLUEPRINTS = [
     'core',
 ]
-
-
-
-
