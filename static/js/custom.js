f70d2caed456b65f70302146822a414787d93500
Fachri Hawari <fachri.hawari@gmail.com>
2018-08-07 14:27:07 +0700

    fix error when js not called

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,7 +1,9 @@
-$('.icp').iconpicker({
-    placement: 'right'
-});
-
-$('.icp').on('iconpickerSelected', function(event){
-    $(this).val('fa ' + event.iconpickerValue)
-});
+if ($.prototype.iconpicker) {
+    $('.icp').iconpicker({
+        placement: 'right'
+    });
+    
+    $('.icp').on('iconpickerSelected', function(event){
+        $(this).val('fa ' + event.iconpickerValue)
+    });
+}
